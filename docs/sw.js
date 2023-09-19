322188810e47ae4f5b12f9d27cf3f34cd58f3992
Bruno Cassol <brunocassol@gmail.com>
2023-09-19 18:35:28 -0400

    service worker testing

---
+++ docs/sw.js
@@ -0,0 +1,25 @@
+// Installing Service Worker
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install', e);
+});
+
+// // Fetching content using Service Worker
+// self.addEventListener('fetch', (e) => {
+//     // Cache http and https only, skip unsupported chrome-extension:// and file://...
+//     if (!(
+//        e.request.url.startsWith('http:') || e.request.url.startsWith('https:')
+//     )) {
+//         return; 
+//     }
+
+//   e.respondWith((async () => {
+//     const r = await caches.match(e.request);
+//     console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+//     if (r) return r;
+//     const response = await fetch(e.request);
+//     const cache = await caches.open(cacheName);
+//     console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+//     cache.put(e.request, response.clone());
+//     return response;
+//   })());
+// });
